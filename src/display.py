c9690612cad5843e490aa1faa956fb74c1a99020
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-20 11:05:42 +0530

    ASCII art done

---
+++ src/display.py
@@ -0,0 +1,17 @@
+from src.ascii_art import BOMB_STATES
+
+
+def rule_book():
+    pass
+
+def menu():
+    pass
+
+def active_bome():
+    pass
+
+def bomb_blas():
+    pass
+
+def defused_bomb():
+    pass
