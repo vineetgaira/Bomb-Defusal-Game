84aef9656dce8fd8ba0880831ff3ab8dbddb64c0
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-22 11:04:42 +0530

    Main loop skeleton create

--- src/game.py
+++ src/game.py
@@ -1,10 +1,11 @@
 import time
 import random
 from src.display import rule_book, menu, active_bomb, defused_bomb, bomb_blast,goodbye
-from src.user_input import get_player_choice
+from src.user_input import get_player_choice, enter_code
 from src.utils import clear_screen
 from src.puzzle import sum_puzzle, digit_pattern, reversed_number, mul_puzzle, word_to_number, binary
 from src.timer import show_timer
+
 def start_game():
     correct_wire = random.choice(["RED", "BLUE"])
     saved_code = None
@@ -32,4 +33,41 @@ def start_game():
             game_over = True
         
         if choice == 2:
-            
+            if correct_wire == "BLUE":
+                defused_bomb()
+            game_over = True
+        
+        if choice == 3:
+            clear_screen()
+            active_bomb()
+            if saved_code is None:
+                clue_text, saved_code = random_puzzle()
+            else:
+                clue_text = get_same_clue_again()
+            show_hint(clue_text)
+            time_remaining -= inspect_time_cost
+
+            wait_for_input_to_continue()
+
+        if choice == 4:
+            if saved_code is None:
+                 print("You haven't inspected the bomb yet.")
+            else:
+                guess = enter_code()
+
+                if guess == saved_code:
+                    defused_bomb()
+                    game_over = True
+                else:
+                    bomb_blast()
+                    game_over = True
+
+        if choice == 5:
+            clear_screen()
+            rule_book()
+            wait_for_input_to_continue()
+
+        if choice == 6:
+            goodbye()
+            game_over = True
+
